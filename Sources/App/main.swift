c212ba96a7d69f442106263847a8f83e7ab3c543
twof <fabiobean2@gmail.com>
2017-03-10 10:20:55 -0800

    Added buzz route

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -4,7 +4,7 @@ import HTTP
 let drop = Droplet()
 
 drop.get("/fizz") { request in
-    if let num = request.query?["q"]?.int{
+    if let num = request.query?["q"]?.int {
         var retString = ""
         
         for i in 1...num{
@@ -17,6 +17,7 @@ drop.get("/fizz") { request in
             }else{
                 retString.append(String(i))
             }
+            
             if i != num{
                 retString.append(", ")
             }
@@ -30,6 +31,34 @@ drop.get("/fizz") { request in
     }else{
         let retJSON = try JSON(node: [
                 "error": "Use the \'q\' parameter"
+            ])
+        
+        return try Response(status: .badRequest, json: retJSON)
+    }
+}
+
+drop.get("/buzz") { request in
+    if let num = request.query?["q"]?.int {
+        var retString = ""
+       
+        if num % 3 == 0 && num % 5 == 0 {
+            retString = "FizzBuzz"
+        }else if num % 3 == 0 {
+            retString = "Fizz"
+        }else if num % 5 == 0 {
+            retString = "Buzz"
+        }else{
+            retString = String(num)
+        }
+        
+        let retJSON = try JSON(node: [
+            "val": retString
+        ])
+        
+        return try Response(status: .ok, json: retJSON)
+    }else{
+        let retJSON = try JSON(node: [
+            "error": "Use the \'q\' parameter"
             ])
         
         return try Response(status: .badRequest, json: retJSON)
